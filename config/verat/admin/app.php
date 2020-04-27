0c2f5508e213c95ceaaefba75b0e21ddd8cff942
kodnificent <mbamaravictor@gmail.com>
2020-04-27 04:25:29 +0100

    install verat admin

---
+++ config/verat/admin/app.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'base_uri'          =>  'verat',
+
+    'name'              =>  'Verat Admin',
+
+    'collections'        =>  [
+        //App\Verat\Admin\Collections\Post::class
+    ],
+];
---
+++ config/verat/admin/thumbnailer.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'base_uri'          =>  'verat/thumbnails',
+
+    'filters'           =>  [
+        //App\Verat\Admin\ThubmnailFilters\Small::class
+    ],
+
+    'directories'             =>  [
+        //storage/uploads/imgs,
+        //public_path('imgs'),
+    ],
+
+    'lifetime'               =>  86400
+];

c9fd3c948c1d64f1bd76721f87bbf053f7198080
kodnificent <mbamaravictor@gmail.com>
2020-06-26 08:54:12 +0100

    fix: show analytics script when app is in production

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -13,7 +13,7 @@
         <link rel="stylesheet" href="{{ asset('css/vendor.css') }}">
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
-        @if (env('production'))
+        @if (app()->environment('production'))
             <script data-ad-client="ca-pub-9135565255758714" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
             <!-- Global site tag (gtag.js) - Google Analytics -->
             <script async src="https://www.googletagmanager.com/gtag/js?id=UA-170743507-1"></script>
